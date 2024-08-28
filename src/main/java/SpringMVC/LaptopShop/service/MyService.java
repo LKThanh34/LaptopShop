dc441cb2f94b6685690295bf5bfe5dc78e8407a9
LeKimThanh <lekimthanhx34@gmail.com>
2024-08-29 01:54:17 +0700

    ver2(53)

--- src/main/java/SpringMVC/LaptopShop/service/MyService.java
+++
@@ -1,8 +0,0 @@
-package SpringMVC.LaptopShop.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class MyService {
-
-}
---
+++ src/main/java/SpringMVC/LaptopShop/service/UserService.java
@@ -0,0 +1,11 @@
+package SpringMVC.LaptopShop.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public String handleHello(){
+        return "Hello from service";
+    }
+}
